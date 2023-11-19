60e282a52f9d541ab438cebf1a8966441c7ef460
kimvideo <02dongyoung@gmail.com>
2023-11-19 14:56:13 +0900

    firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-parcelize")
+
+    //firebase
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -46,10 +49,16 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-auth:22.3.0")
+    implementation("com.google.firebase:firebase-firestore:24.9.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("androidx.cardview:cardview:1.0.0")
     implementation( "com.airbnb.android:lottie:6.1.0")
+
+    //firebase
+    implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
+    implementation("com.google.firebase:firebase-analytics")
 
 }
--- app/src/main/java/com/enomusence/projectgm/SignUpActivity.kt
+++ app/src/main/java/com/enomusence/projectgm/SignUpActivity.kt
@@ -3,9 +3,16 @@ package com.enomusence.projectgm
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
@@ -14,6 +21,49 @@ class SignUpActivity : AppCompatActivity() {
         val signUpCancelButton = findViewById<Button>(R.id.signUpCancel)
         signUpCancelButton.setOnClickListener {
             startActivity(signUpCancel)
+        }
+        auth = FirebaseAuth.getInstance()
+
+        val joinBtn = findViewById<Button>(R.id.signUpCheck)
+        joinBtn.setOnClickListener {
+            val email = findViewById<EditText>(R.id.newId)
+            val password = findViewById<EditText>(R.id.newPw)
+            val username = findViewById<EditText>(R.id.userName)
+
+            auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign up success, update UI with the signed-in user's information
+                        val user = auth.currentUser
+
+                        // Firebase Authentication을 통해 얻은 UID
+                        val userId = user?.uid
+
+                        // Firestore에 저장할 사용자 정보
+                        val userMap = hashMapOf(
+                            "email" to email.text.toString(),
+                            "password" to password.text.toString(),
+                            "username" to username.text.toString()
+                        )
+
+                        // Firebase Authentication UID를 사용하여 Firestore에 저장
+                        val db = FirebaseFirestore.getInstance()
+                        db.collection("User")
+                            .document(userId!!)
+                            .set(userMap)
+                            .addOnSuccessListener {
+                                Log.d("JoinActivity", "DocumentSnapshot added with ID: $userId")
+                                Toast.makeText(this, "회원가입 성공", Toast.LENGTH_LONG).show()
+                            }
+                            .addOnFailureListener { e ->
+                                Log.w("JoinActivity", "Error adding document", e)
+                                Toast.makeText(this, "회원가입 실패", Toast.LENGTH_LONG).show()
+                            }
+                    } else {
+                        // If sign up fails, display a message to the user.
+                        Toast.makeText(this, "회원가입 실패", Toast.LENGTH_LONG).show()
+                    }
+                }
         }
     }
 }
